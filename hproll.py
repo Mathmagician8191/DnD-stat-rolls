9ed9754bd59ae19ec2e89ac5bb7e6907a994d95e
Vexnos <88638499+Vexnos@users.noreply.github.com>
2022-08-18 18:04:48 +1200

    Added stuff like Constitution Modifier

--- hproll.py
+++ hproll.py
@@ -3,9 +3,15 @@ from random import randint
 while True:
     hit_die = int(input("What's your hit die? "))
 
+    con_mod = int(input("What's your constitution modifier? "))
+
     hp_roll = randint(1,hit_die)
 
-    print(hp_roll)
+    max_hp = hit_die + con_mod
+
+    print(f"Max HP: {max_hp}")
+    print(f"Base HP roll: {hp_roll}")
+    print("Roll with constitution modifier: " + str(hp_roll + con_mod))
 
     roll_again = input("Roll again? (y/n) ")
     if not roll_again.lower().startswith("y"):
