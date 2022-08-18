e99916b76ddd198f71b467da848b90b16db29ccd
Vexnos <88638499+Vexnos@users.noreply.github.com>
2022-08-19 08:53:25 +1200

    Added average HP

--- hproll.py
+++ hproll.py
@@ -7,9 +7,12 @@ while True:
 
     hp_roll = randint(1,hit_die)
 
+    avg_hp = (hit_die / 2) + 1
+
     max_hp = hit_die + con_mod
 
     print(f"Max HP: {max_hp}")
+    print(f"Average HP: {avg_hp}")
     print(f"Base HP roll: {hp_roll}")
     print("Roll with constitution modifier: " + str(hp_roll + con_mod))
 
